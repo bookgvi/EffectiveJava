c1c73b18b432a6723ec026138ce5d28ab40e51bd
bookgvi <bookgvi@gmail.com>
2021-08-18 00:41:15 +0300

    change class name Permutation -> Subsets

--- src/Algos/Subsets.java
+++ src/Algos/Subsets.java
@@ -2,15 +2,15 @@ package Algos;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Permutations {
+public class Subsets {
     private static final List<Integer> subset = new ArrayList<>();
     private static final List<List<Integer>> subsetsList = new ArrayList<>();
     private static final int SET_SIZE = 3;
 
     public static void main(String[] args) {
         search(1);
+        System.out.printf("Кол-во подмножеств = %d\n %s", subsetsList.size(), subsetsList);
     }
 
     private static void search(int k) {
