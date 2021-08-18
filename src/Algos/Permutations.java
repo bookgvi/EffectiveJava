f8a0a3fa3515272196f693cc9a19c0b2910e9c72
bookgvi <bookgvi@gmail.com>
2021-08-18 17:30:12 +0300

    Permutation algo with recursion

---
+++ src/Algos/Permutations.java
@@ -0,0 +1,33 @@
+package Algos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    private final static List<Integer> permutation = new ArrayList<>();
+    private final static List<List<Integer>> permutationsList = new ArrayList<>();
+    private final static List<Integer> list = List.of(0, 1, 2);
+    private static final List<Boolean> isPresent = new ArrayList<>(List.of(false, false, false));
+
+    public static void main(String[] args) {
+        search();
+        System.out.printf("Кол-во множеств = %d\n %s", permutationsList.size(), permutationsList);
+    }
+
+    private static void search() {
+        if (permutation.size() == list.size()) {
+            permutationsList.add(List.copyOf(permutation));
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (isPresent.get(i)) continue;
+            isPresent.set(i, true);
+            permutation.add(i);
+            search();
+            isPresent.set(i, false);
+            int j = i;
+            if (permutation.size() - 1 < i) j = Math.min(permutation.size() - 1, i);
+            permutation.remove(j);
+        }
+    }
+
+}
