2594a3e184aeeed7428f1062f1b40173e27d8771
bookgvi <bookgvi@gmail.com>
2021-09-11 12:24:13 +0300

    ReverseStream

---
+++ src/Leetcode/Simple/ReverseString.java
@@ -0,0 +1,19 @@
+package Leetcode.Simple;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Мама мыла   раму";
+        String strReversed = new StringBuilder(str).reverse().toString();
+        System.out.printf("%s, %d\n", str, str.length());
+        System.out.printf("%s, %d\n", strReversed, strReversed.length());
+        System.out.printf("%s, %d\n", reverse(strReversed), reverse(strReversed).length());
+    }
+    private static String reverse(String str) {
+        return Stream.of(str.split(" ", 0)).map(subStr -> new StringBuilder(subStr).reverse().append(" "))
+                .collect(Collectors.joining())
+                .substring(0, str.length());
+    }
+}
