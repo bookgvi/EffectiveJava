91b0bead23411e350f736e8a8560f619c9018519
bookgvi <bookgvi@gmail.com>
2021-09-15 16:43:27 +0300

    string: prifix function extended algo (KMP)

--- src/Algos/String/PIFunction.java
+++ src/Algos/String/PIFunction.java
@@ -9,6 +9,7 @@ public class PIFunction {
     public static void main(String[] args) {
         String str = "abcabcd";
         System.out.println(Arrays.toString(piFunction(str)));
+        System.out.println(Arrays.toString(piFunctionExt(str)));
     }
 
     private static int[] piFunction(String str) {
@@ -22,6 +23,18 @@ public class PIFunction {
                     pi[i] = j + 1;
                 }
             }
+        }
+        return pi;
+    }
+
+    private static int[] piFunctionExt(String str) {
+        int len = str.length();
+        int[] pi = new int[len];
+        for (int i = 1; i < len; i += 1) {
+            int j = pi[i - 1];
+            if (j > 0 && str.charAt(pi[i]) != str.charAt(pi[j])) j = pi[j - 1];
+            if (str.charAt(i) == str.charAt(j)) j += 1;
+            pi[i] = j;
         }
         return pi;
     }
