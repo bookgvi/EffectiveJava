c7be7b9105265faa1f38339981c8107cb04c476d
bookgvi <bookgvi@gmail.com>
2021-09-15 19:43:52 +0300

    fix algo issue for pi-function (prefix-function) algo. Algo Knut-Morris-Pratt find substring in text

--- src/Algos/String/PIFunction.java
+++ src/Algos/String/PIFunction.java
@@ -1,15 +1,33 @@
 package Algos.String;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /*
  * Префиксная функция
  * */
 public class PIFunction {
     public static void main(String[] args) {
-        String str = "abcabcd";
+        String str = "abcaaabcabcd";
         System.out.println(Arrays.toString(piFunction(str)));
         System.out.println(Arrays.toString(piFunctionExt(str)));
+
+        String text = "это разделитель, который не должен нигде более встречаться. Посчитаем для префикс. Теперь рассмотрим её значения, кроме первых\n" +
+                "(которые, как видно, относятся к строке   и разделителю). По определению, значение\n" +
+                "показывает наидлиннейшую длину подстроки, оканчивающейся в позиции   и совпадающего с префиксом. Но в\n" +
+                "нашем случае это   — фактически длина наибольшего блока совпадения со строкой   и оканчивающегося в позиции\n" +
+                "  . Больше, чем   , эта длина быть не может — за счёт разделителя. А вот равенство\n" +
+                "оно достигается), означает, что в позиции   оканчивается искомое вхождение строки     (только не надо забывать, что";
+        String strToFind = "префикс";
+        String delimiter = "#";
+
+        String prepText = (strToFind + delimiter + text);
+        int[] pi = piFunctionExt(prepText);
+        int[] indexes = IntStream.range(0, pi.length).filter(index -> pi[index] == strToFind.length())
+                .map(index -> index - strToFind.length() * 2 + 1).toArray();
+        System.out.printf("Count = %d,\n %s", indexes.length, Arrays.toString(indexes));
     }
 
     private static int[] piFunction(String str) {
@@ -32,7 +50,7 @@ public class PIFunction {
         int[] pi = new int[len];
         for (int i = 1; i < len; i += 1) {
             int j = pi[i - 1];
-            if (j > 0 && str.charAt(pi[i]) != str.charAt(pi[j])) j = pi[j - 1];
+            while (j > 0 && str.charAt(i) != str.charAt(j)) j = pi[j - 1];
             if (str.charAt(i) == str.charAt(j)) j += 1;
             pi[i] = j;
         }
