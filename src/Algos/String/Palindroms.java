85b3308ebe63f784ac9453425d187b1174046d50
bookgvi <bookgvi@gmail.com>
2021-09-17 00:01:11 +0300

    litle refactoring for Manaker algo

--- src/Algos/String/Palindroms.java
+++ src/Algos/String/Palindroms.java
@@ -10,22 +10,22 @@ import java.util.stream.Stream;
 public class Palindroms {
     public static void main(String[] args) {
         String str = "12343211232";
-        int[] manacher = manacherOdds(str);
-        System.out.println(Arrays.toString(manacher));
+        System.out.println(Arrays.toString(manakerOdds(str)));
     }
 
-    private static int[] manacherOdds(String str) {
+    private static int[] manakerOdds(String str) {
         int len = str.length();
-        int[] z = new int[len];
+        int[] m = new int[len];
         for (int i = 1, l = 0, r = 0; i < len; i += 1) {
-            if (i < r) z[i] = Math.min(z[l + r - i], r - i + 1);
-            while (i + z[i] < len && i - z[i] >= 0 && str.charAt(i + z[i]) == str.charAt(i - z[i])) z[i] += 1;
-            if (z[i] + i - 1 > r) {
-                l = i - z[i] + 1;
-                r = i + z[i] - 1;
+            if (i < r) m[i] = Math.min(m[i - l], r - i + 1);
+            while (i - m[i] >= 0 && i + m[i] < len && str.charAt(i - m[i]) == str.charAt(i + m[i]))
+                m[i] += 1;
+            if (i + m[i] - 1 > r) {
+                l = i - m[i] + 1;
+                r = i + m[i] - 1;
             }
         }
-        return z;
+        return m;
     }
 
 }
