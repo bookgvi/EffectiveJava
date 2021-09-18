dc6821b95f834f86f392c9ac8c813f89855c086b
bookgvi <bookgvi@gmail.com>
2021-09-18 15:04:25 +0300

    hanoy tower algo

---
+++ src/Leetcode/Simple/HanoyTower.java
@@ -0,0 +1,20 @@
+package Leetcode.Simple;
+
+public class HanoyTower {
+    public static void main(String[] args) {
+        int n = 3;
+        hanoy(n, 1, 2, 3);
+    }
+
+    private static int greyCode(int n) {
+        return n ^ (n >> 1);
+    }
+
+    private static void hanoy(int n, int from, int middle, int target) {
+        if (n != 0) {
+            hanoy(n - 1, from, target, middle);
+            System.out.printf("%d -> %d; %d\n", from, target, greyCode(n));
+            hanoy(n - 1, middle, from, target);
+        }
+    }
+}
