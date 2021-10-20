aab208e0ad08c7b12c38134c3d4de49016b4fc9b
bookgvi <bookgvi@gmail.com>
2021-10-20 21:25:05 +0300

    algo for suffixArray construction

--- src/Algos/Suffix/SuffixArray.java
+++ src/Algos/Suffix/SuffixArray.java
@@ -18,20 +18,44 @@ public class SuffixArray {
     }
 
     private static int[] lexicoGraphikSuffixSort(String str) {
-        int len = str.length(), b = 8;
-        int[] p = new int[len], c = new int[len], count = new int[1 << b];
-        for (int i = 0; i < len; i += 1) {
-            count[str.codePointAt(i) - firstCharByte + 1] += 1;
-        }
-        for (int i = 1; i < 1 << b; i += 1)
+        int len = str.length();
+        int[] count = new int[1 << 8], p = new int[len], c = new int[len];
+        byte[] strBytes = str.getBytes();
+        int firstCharByte = "a".getBytes()[0];
+        for (int i =0; i < len; i += 1)
+            count[strBytes[i] - firstCharByte + 1] += 1;
+        for (int i = 1; i < 1 << 8; i += 1)
             count[i] += count[i - 1];
-        for (int i = len - 1; i >= 0; i -= 1) {
-            p[--count[str.codePointAt(i) - firstCharByte + 1]] = i;
-        }
+        for (int i = len - 1; i >=0; i -= 1)
+            p[--count[strBytes[i] - firstCharByte + 1]] = i;
+
         int classes = 1;
         for (int i = 1; i < len; i += 1) {
             if (str.charAt(p[i - 1]) != str.charAt(p[i])) classes += 1;
             c[p[i]] = classes - 1;
+        }
+
+        int[] pn = new int[len], cn = new int[len];
+        for (int h = 0; (1 << h) < len; h += 1) {
+            count = new int[1 << 8];
+            for (int i = 0; i < len; i += 1) {
+                pn[i] = p[i] - (1 << h);
+                if (pn[i] < 0) pn[i] += len;
+            }
+            for (int i = 0; i < len; i += 1)
+                count[c[pn[i]]] += 1;
+            for (int i = 1; i < 1 << 8; i += 1)
+                count[i] += count[i - 1];
+            for (int i = len - 1; i >= 0; i -= 1)
+                p[--count[c[pn[i]]]] = pn[i];
+            cn[p[0]] = 0;
+            classes = 1;
+            for (int i = 1; i < len; i += 1) {
+                int mid1 = (p[i] + (1 << h)) % len, mid2 = (p[i - 1] + (1 << h)) % len;
+                if (c[p[i]] != c[p[i - 1]] || c[mid1] != c[mid2]) classes += 1;
+                cn[p[i]] = classes - 1;
+            }
+            System.arraycopy(cn, 0, c, 0, len);
         }
         return p;
     }
