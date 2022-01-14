bd2dcf4c9b5eb05a9d1ade3fd7651c059aaedd8b
bookgvi <bookgvi@gmail.com>
2022-01-14 19:51:14 +0300

    some tests for src/Leetcode/Simple/StringToInteger.java

--- src/Leetcode/Simple/StringToInteger.java
+++ src/Leetcode/Simple/StringToInteger.java
@@ -1,22 +1,22 @@
 package Leetcode.Simple;
 
 /*
-*
-* https://leetcode.com/problems/string-to-integer-atoi/submissions/
-* */
+ *
+ * https://leetcode.com/problems/string-to-integer-atoi/submissions/
+ * */
 public class StringToInteger {
-    public int myAtoi(String s) {
+    public static int myAtoi(String s) {
         int index = 0, n = s.length(), sign = 1;
         long res = 0;
         if (n < 1) return 0;
-        while(index < n && s.charAt(index) == ' ') index += 1;
+        while (index < n && s.charAt(index) == ' ') index += 1;
         if (index < n && s.charAt(index) == '-') {
             sign = -1;
             index += 1;
         } else if (index < n && s.charAt(index) == '+') {
             index += 1;
         }
-        while(index < n && s.charAt(index) >= '0' && s.charAt(index) <= '9') {
+        while (index < n && s.charAt(index) >= '0' && s.charAt(index) <= '9') {
             int digit = s.charAt(index) - '0';
             res = res * 10 + digit;
             if (res > Integer.MAX_VALUE && sign == 1) {
@@ -28,6 +28,12 @@ public class StringToInteger {
             }
             index += 1;
         }
-        return (int) res * sign;
+        res = (int) res * sign;
+        return (int) res;
+    }
+
+    public static void main(String[] args) {
+        String n = "-919293945959595959595";
+        int res = myAtoi(n);
     }
 }
