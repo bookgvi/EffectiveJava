387fd0455af74534d5595d379d51e119e528f404
bookgvi <bookgvi@gmail.com>
2022-02-20 11:13:00 +0300

    leetcode CombinationSum task another solution (Accepted)

--- src/Leetcode/Simple/CombinationSum.java
+++ src/Leetcode/Simple/CombinationSum.java
@@ -5,11 +5,68 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /*
-* https://leetcode.com/problems/combination-sum/submissions/
-*
-* это решение не подходит по причине лексикографической сортировки массивов (идите нахер с такими требованиями :D))
-* */
-public class CombinationSum {
+ * https://leetcode.com/problems/combination-sum/submissions/
+ *
+ * Accepted
+ * */
+class CombinationSumSolution {
+    private static int n, len, sum;
+    private static List<Integer> nextCombination, prevIndexes;
+    private static List<List<Integer>> allCombinations;
+
+    public static void main(String[] args) {
+        int[] nums = {2};
+        int target = 1;
+        List<List<Integer>> res = combinationSum(nums, target);
+    }
+
+    public static List<List<Integer>> combinationSum(int[] nums, int target) {
+        n = target;
+        len = nums.length;
+        nextCombination = new ArrayList<>();
+        prevIndexes = new ArrayList<>();
+        allCombinations = new ArrayList<>();
+        backTrackSumm(nums);
+        return allCombinations;
+    }
+
+    private static void backTrackSumm(int[] nums) {
+        for (int i = 0; i < len; i += 1) {
+            if (check(nums, i)) {
+                store(nums, i);
+                if (sum == n)
+                    allCombinations.add(List.copyOf(nextCombination));
+                else if (sum < n)
+                    backTrackSumm(nums);
+                remove();
+            }
+        }
+    }
+
+    private static boolean check(int[] nums, int curIndex) {
+        if (prevIndexes.size() == 0) return true;
+        return curIndex >= prevIndexes.get(prevIndexes.size() - 1) && sum + nums[curIndex] <= n;
+    }
+
+    private static void store(int[] nums, int curIndex) {
+        nextCombination.add(nums[curIndex]);
+        sum += nums[curIndex];
+        prevIndexes.add(curIndex);
+    }
+
+    private static void remove() {
+        int prevVal = nextCombination.remove(nextCombination.size() - 1);
+        sum -= prevVal;
+        prevIndexes.remove(prevIndexes.size() - 1);
+    }
+}
+
+/*
+ * https://leetcode.com/problems/combination-sum/submissions/
+ *
+ * это решение не подходит по причине лексикографической сортировки массивов (идите нахер с такими требованиями :D))
+ * */
+class CombinationSum {
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> listOfFragments = new ArrayList<>();
         List<Integer> fragment = IntStream.range(0, target).mapToObj(i -> 1).collect(Collectors.toList());
@@ -31,17 +88,17 @@ public class CombinationSum {
     }
 
     private static boolean checkFragment(int[] candidates, List<Integer> fragment) {
-       Map<Integer, Boolean> include = new HashMap<>();
-       for (int elt : fragment) include.putIfAbsent(elt, false);
-       for (int elt : candidates) {
-           if (include.containsKey(elt)) {
-               include.put(elt, true);
-           }
-       }
-       boolean res = true;
-       for (boolean isPresent : include.values())
-           res &= isPresent;
-       return res;
+        Map<Integer, Boolean> include = new HashMap<>();
+        for (int elt : fragment) include.putIfAbsent(elt, false);
+        for (int elt : candidates) {
+            if (include.containsKey(elt)) {
+                include.put(elt, true);
+            }
+        }
+        boolean res = true;
+        for (boolean isPresent : include.values())
+            res &= isPresent;
+        return res;
     }
 
     private static List<List<Integer>> minCombinationSum(int[] candidates, int target) {
