255d776e16c753d407733edbddaeb9e472e26c8a
bookgvi <bookgvi@gmail.com>
2022-12-02 10:28:07 +0300

    enum triks

---
+++ src/Structures/Enum/IExtMathOperation.java
@@ -0,0 +1,7 @@
+package Structures.Enum;
+
+public interface IExtMathOperation {
+    default double apply_ext(double x, double y, double z) {
+        return 0;
+    };
+}
--- src/Structures/Enum/MathOperationImpl.java
+++ src/Structures/Enum/MathOperationImpl.java
@@ -1,6 +1,6 @@
 package Structures.Enum;
 
-enum MathOperationImpl implements IMathOperation {
+enum MathOperationImpl implements IMathOperation, IExtMathOperation {
     PLUS("+") {
         public double apply(double x, double y) {
             return x + y;
@@ -14,6 +14,16 @@ enum MathOperationImpl implements IMathOperation {
     MULTIPLY("*") {
         public double apply(double x, double y) {
             return x * y;
+        }
+    },
+    MULTIPLY_EXT("*") {
+        @Override
+        public double apply(double x, double y) {
+            return x * y;
+        }
+
+        public double apply_ext(double x, double y, double z) {
+            return x * y * z;
         }
     },
     DIVIDE("/") {
@@ -40,15 +50,22 @@ class App {
         IMathOperation minus = MathOperationImpl.MINUS;
         IMathOperation multiply = MathOperationImpl.MULTIPLY;
         IMathOperation divide = MathOperationImpl.DIVIDE;
-        double x = 21, y = 2;
+        IExtMathOperation extMul = MathOperationImpl.MULTIPLY_EXT;
+        double x = 21, y = 2, z = 2;
         calcAndDisplay(plus, x, y);
         calcAndDisplay(minus, x, y);
         calcAndDisplay(multiply, x, y);
         calcAndDisplay(divide, x, y);
+        calcAndDisplay(extMul, x, y, z);
     }
 
     private static void calcAndDisplay(IMathOperation op, double x, double y) {
         double res = op.apply(x, y);
         System.out.printf("%.2f %s %.2f = %.2f\n", x, op, y, res);
+    }
+
+    private static void calcAndDisplay(IExtMathOperation op, double x, double y, double z) {
+        double res = op.apply_ext(x, y, z);
+        System.out.printf("%.2f %s %.2f %s %.2f = %.2f\n", x, op, y, op, z, res);
     }
 }
